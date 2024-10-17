8598aff7433ef5a307cfb4e686630adeb5c632d3
LiamSaul-Sky <liam.saul@sky.uk>
2024-10-17 11:46:51 +0100

    Added all bulbs to one variable

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 // Write your code here
 console.log("Hello World");
 
-const bulb1 = document.querySelector("#lightbulb1");
-const bulb2 = document.querySelector("#lightbulb2");
-const bulb3 = document.querySelector("#lightbulb3");
 const bulbs = document.querySelectorAll(".item.lightbulb")
 const subtitle = document.querySelector(".subtitle");
 let count = 0;
-
-// bulb1.addEventListener("mouseover", lightUp);
-// bulb2.addEventListener("mouseover", lightUp);
-// bulb3.addEventListener("mouseover", lightUp);
 
 for (let i =0;i < bulbs.length; i++){
     console.log(bulbs[i]);
